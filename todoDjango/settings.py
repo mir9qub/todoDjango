d4a685e7bd328535e8298be6718c38abbba16557
mir9qub <86770092+mir9qub@users.noreply.github.com>
2023-01-23 13:44:42 +0500

    Update settings.py

--- todoDjango/settings.py
+++ todoDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ceha$obd_5=-djj8+6(+=%0enac1tyt0_($##uu#&$3=hrv850'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
